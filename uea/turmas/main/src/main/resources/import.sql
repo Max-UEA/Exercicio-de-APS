032c5c8fa40eaaae85a361afa555d86850e732d1
Max-UEA <mwmc.lic22@uea.edu.br>
2024-12-05 08:42:34 -0400

    Atualizou o repositório

---
+++ uea/turmas/main/src/main/resources/import.sql
@@ -0,0 +1,15 @@
+INSERT INTO CURSO(NOME) VALUES ('Ciência da Computação');
+INSERT INTO CURSO(NOME) VALUES ('Sistemas de Informação');
+
+INSERT INTO PROFESSOR(NOME, EMAIL) VALUES ('Prof. Carlos', 'carlos@universidade.com');
+INSERT INTO PROFESSOR(NOME, EMAIL) VALUES ('Prof. Beatriz', 'beatriz@universidade.com');
+
+INSERT INTO TURMA(NOME, CURSO_ID, PROFESSOR_ID) VALUES  ('Turma Alpha', 1, 1);
+INSERT INTO TURMA(NOME, CURSO_ID, PROFESSOR_ID) VALUES ('Turma Beta', 2, 2);
+
+INSERT INTO Estudante (NOME, ID, EMAIL) VALUES ('Laura Dias', 1, 'laura.dias@teste.com');
+INSERT INTO Estudante (NOME, ID, EMAIL) VALUES ('Gabriel Sousa', 2, 'gabriel.sousa@teste.com');
+INSERT INTO Estudante (NOME, ID, EMAIL) VALUES ('Isabela Rocha', 3, 'isabela.rocha@teste.com');
+INSERT INTO Estudante (NOME, ID, EMAIL) VALUES ('Lucas Oliveira', 4, 'lucas.oliveira@teste.com');
+INSERT INTO Estudante (NOME, ID, EMAIL) VALUES ('Sofia Mendes', 5, 'sofia.mendes@teste.com');
+INSERT INTO Estudante (NOME, ID, EMAIL) VALUES ('Henrique Costa', 6, 'henrique.costa@teste.com');
