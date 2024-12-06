31495b6a850c06b754618fbc50deee8c86f662cd
Max-UEA <mwmc.lic22@uea.edu.br>
2024-12-06 14:08:16 -0400

    Atualizei o repositório

--- uea/turmas/main/src/main/java/com/gerenciadordeturmas/main/repository/ProfessorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gerenciadordeturmas.main.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.gerenciadordeturmas.main.model.Professor;
-
-public interface ProfessorRepository extends JpaRepository<Professor, Long> {
-    
-}
--- uea/turmas/main/src/main/java/com/gerenciadordeturmas/main/repository/TurmaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gerenciadordeturmas.main.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.gerenciadordeturmas.main.model.Turma;
-
-public interface TurmaRepository extends JpaRepository<Turma, Long> {}
